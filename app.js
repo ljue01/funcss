c96996531bf23c2d66755234c3fef0f7adea081d
MacBook <>
2023-05-08 00:50:55 +0800

    upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// by Just. Liu
+// v0.0.1
+// 2020.12.01 p.m. 9:44
+
+const app = Vue.createApp({
+  data() {
+    return {
+      buttons: [],
+			inputs: []
+    }
+  },
+  mounted() {
+		axios.all([
+			axios.get('data/buttons.json'),
+			axios.get('data/inputs.json')
+		]).then(axios.spread((post1, post2) => {
+			this.buttons = post1.data;
+			this.inputs = post2.data;
+		})).catch(error => {
+			console.log(error);
+		});
+	}
+})
+
+app.mount('#app')
