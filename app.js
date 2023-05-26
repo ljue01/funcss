87a818c22fcbc273ef83ceacc2fc5f37a06c8be4
ljue01 <liujeyi@liujueyi.cn>
2023-05-26 10:29:01 +0800

    优化字体cdn

--- app.js
+++ app.js
@@ -122,21 +122,4 @@ const app = Vue.createApp({
   }
 })
 
-app.component('footers', {
-  template: `
-	<footer>
-		<section>
-			<span>funcss ©️ 2020</span>
-			<a class="text-link" target="_blank" href="https://beian.miit.gov.cn/">冀ICP备19017590号-1</a>
-		</section>
-		<section>
-			<a class="text-link" href="javascript:;"  @click="showLike">喜欢</a>
-			<a class="text-link" href="javascript:;"  @click="showAbout">关于</a>
-			<a class="text-link" href="https://whtool.liujueyi.cn/" target="_blank">WHTOOL</a>
-			<a class="text-link" href="https://gitee.com/ctrl-S/just-web-dev" target="_blank">Gitee</a>
-		</section>
-	</footer>
-	`
-})
-
 app.mount('#app')
